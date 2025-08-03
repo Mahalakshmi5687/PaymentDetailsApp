54a4b6cdcc7299e443da3f8ed254db9521bb392e
MahalakshmiMusanalli-gep <140727497+MahalakshmiMusanalli-gep@users.noreply.github.com>
2025-08-03 09:51:43 +0530

    ++

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    //to convert this class to dbcontext class we inherit the dbcontext class from parent class
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
